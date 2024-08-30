9720133ff4f76f0824e170c54f9d18b0253165d3
meohyeon <theogus2@naver.com>
2024-08-30 20:56:54 +0900

    요청제한 늘리기

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ const __dirname = path.resolve()
 app.set('port', process.env.PORT || 3000);
 app.use(morgan ('dev')); // 개발 => dev, 배포 => combined
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true})); //extended 는 중첩된 객체표현을 허용할지 말지 정함 객체 안에 객체를 파싱할 수 있게하려면 true. true = qs, false querystring
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({extended : true,limit: '50mb'})); //extended 는 중첩된 객체표현을 허용할지 말지 정함 객체 안에 객체를 파싱할 수 있게하려면 true. true = qs, false querystring
 app.use(cors());
 
 app.use("/",router);
