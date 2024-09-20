d222c617d739d750cfbdd1fb7bbe83920b36d78b
meohyeon <theogus2@naver.com>
2024-09-20 18:06:34 +0900

    11

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -12,7 +12,7 @@ const reviewController = {
             for (let i = 0; i < 6; i++) {
                 seoulTime += chars.charAt(Math.floor(Math.random() * chars.length));
             }
-            const button = {attachment:{"button":[{"name":"채널 추가","type":"WL"}]}}
+            const button = `{"attachment":{"button":[{"name":"채널 추가","type":"WL"}]}}`
             console.log(button)
             const text = "안녕하세요, 고객님.\nDS오토라인입니다!\n\n고객님께서 저희 DS오토라인을 선택해주신 것에 진심으로 감사드립니다.\n고객님의 신뢰에 보답하기 위해 신속하고 정확한 상담을 제공하겠습니다.\n\n고객님의 요청에 따라 전문 카멘토가 곧 기입하신 연락처로 연락드릴 예정입니다.\n상담 중 궁금한 점이나 추가적인 요청 사항이 있으시면 언제든지 편하게 말씀 부탁드립니다.\n\n저희는 고객님의 만족을 최우선으로 생각하며, 믿음직한 서비스를 약속드립니다.\n감사합니다."
             const data = new FormData()
@@ -22,6 +22,7 @@ const reviewController = {
             data.append('receiverTelNo','01036435995')
             data.append('userKey',seoulTime)
             data.append('attachment',button)
+            console.log(JSON.parse(button))
             const response = await axios.post("https://apimsg.wideshot.co.kr/api/v2/message/alimtalk",data,{
                 headers: {'Content-Type':'multipart/form-data','sejongApiKey':process.env.SEJONG_API_KEY}
             })
